93c12d586b43c70e8748827738c7c13ef9790f0a
Rafał Jaworski <rjaworski@sii.pl>
2019-06-05 12:35:03 +0200

    prettier config add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { observer } from "mobx-react";
-import * as React from "react";
-import ToDoList from "./components/toDoList/ToDoList";
-import listStore from "./stores/listStore";
+import { observer } from 'mobx-react';
+import * as React from 'react';
+import ToDoList from './components/toDoList/ToDoList';
+import listStore from './stores/listStore';
 
 @observer
 export default class App extends React.Component {
-  private store: listStore;
+    private store: listStore;
 
-  constructor(props: any) {
-    super(props);
-    this.store = new listStore();
-  }
+    constructor(props: any) {
+        super(props);
+        this.store = new listStore();
+    }
 
-  componentDidMount() {
-    this.store.loadItems();
-  }
+    componentDidMount() {
+        this.store.loadItems();
+    }
 
-  render() {
-    return (
-      <div>
-        <ToDoList toDoList={this.store.itemList} />;
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ToDoList toDoList={this.store.itemList} />;
+            </div>
+        );
+    }
 }
